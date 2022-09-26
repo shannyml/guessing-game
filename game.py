cf2ced2cc5d1ca6f2a7c2dd02fb569b1376db308
Shannen <shannen.yml@gmail.com>
2022-09-26 14:46:27 -0700

    Guessing Game

--- game.py
+++ game.py
@@ -1,4 +1,24 @@
 """A number-guessing game."""
-print("hi, player")
-# Put your code here
+import random
 
+#ask user for name
+#greet player with name
+name = input("What is your name?")
+print("Hi,", name)
+guess_num_input = input("Guess a number: ")
+guess_num = int(guess_num_input)
+
+nums = random.randint(0, 100)
+
+while guess_num != nums:
+
+    if guess_num > nums:
+        print("Number too high, guess again!")
+        guess_num = int(input("Guess another number: "))
+    elif guess_num < nums:
+        print("Number too low, guess again!")
+        guess_num = int(input("Guess another number: "))
+print("You did it! The number is", nums)
+#prompt user to guess a number
+#cycle through until user guesses the right number
+#end when user guesses the number
